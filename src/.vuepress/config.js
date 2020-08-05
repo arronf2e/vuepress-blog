7a3b39f96484954920ff01dc4a8991ce88cc4f3f
zhanghao5 <zhanghao5@yy.com>
2020-08-05 16:48:58 +0800

    create

---
+++ src/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'My Blog',
+  description: 'This is my blog',
+  theme: 'meteorlxy',
+  themeConfig: {
+    lang: 'zh-CN',
+    // 或者
+    lang: require('vuepress-theme-meteorlxy/lib/langs/zh-CN'),
+  },
+}
